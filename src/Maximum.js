632f8eca08ffdd3814eadabc4ed5ecff1d852bf5
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-14 15:47:10 +0530

    using usestate
    add two number,max value,subtract,

--- src/Maximum.js
+++ src/Maximum.js
@@ -2,18 +2,24 @@ import React, { useState } from "react";
 function Maximum() {
 
     const [a, setA] = useState(0);
-    const[b,setB]=useState(0);
-    const[max,setMax]=useState(0);
-    
+    const [b, setB] = useState(0);
+    const [max, setMax] = useState(0);
+
     return (
         <>
             <h1>A: {a}</h1>
             <h1>B:{b}</h1>
-            <h1>Max is {max? a:b}</h1>
+            <h1>Max Value is:{max ? a : b}</h1>
+            <h1>Subtract value is: {max}</h1>
+            <h1>Add number:{max}</h1>
 
-            <button onClick={()=>setA(a+1)}>Increment</button>
-             <button onClick={()=>setB(b+1)}>Dcrement</button>
-              <button onClick={()=>setMax(a>b)}>Update MaxValue</button>
+
+            <button onClick={() => setA(a + 1)}>Increment</button>
+            <button onClick={() => setB(b + 1)}>Dcrement</button>
+            <button onClick={() => setMax(a > b)}>Update MaxValue</button>
+            <button onClick={() => setMax(a + b)}>Add Number</button>
+            <button onClick={() => setMax(a - b)}>Update Subtract</button>
+
 
 
         </>
