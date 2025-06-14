a060739893de0cfcf692868a288c4cb51525df80
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-14 16:17:19 +0530

    Create a single Hook that holds an object

--- src/Car.js
+++ src/Car.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 
 function Car(){
-const [name, setName] = useState("Fortuner");
-const [color, setColor] = useState("Blue");
-const [company, setCompany] = useState("Toyata");
-const [year, setYear] = useState("2025");
+const [taxi, setTaxi] = useState({Name:"Safari",Color: "White", Company:"Tata", Model :"2025"});
 return(
-    <>
-    <h1>My Car name is:{name}</h1>
-     <h1>Color is:{color} </h1>
-      <h1>company name is: {company}</h1>
-      <h1>Car model is {year}</h1>
+    <><h1>My Car name is:{taxi.Name}</h1>
+     <h1>Color is:{taxi.Colorcolor} </h1>
+      <h1>company name is: {taxi.Company}</h1>
+      <h1>Car's model {taxi.Model}</h1>
      </>
 );
 }
