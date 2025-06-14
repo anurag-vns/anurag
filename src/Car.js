04ae4ce2318e8f96c27724de3eb9914102518cf8
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-14 17:04:37 +0530

    usestate with spread operator
    Use the spread operator to update only the color of the car:

--- src/Car.js
+++ src/Car.js
@@ -2,11 +2,30 @@ import React, { useState } from "react";
 
 function Car(){
 const [taxi, setTaxi] = useState({Name:"Safari",Color: "White", Company:"Tata", Model :"2025"});
+
+/*const UpdateColor=()=>{
+    setTaxi(previousState=>{
+        return{...previousState,Color:"black"}
+    });
+
+    const updateColor = (newColor) => {
+    setTaxi({ ...taxi, color: newColor });
+  }*/
+
+    /*function updateColor()
+    {
+        setTaxi(taxi)
+    }*/
+   const updateColor= () =>{
+    setTaxi({...taxi, Color:"Blue"});
+   }
+
 return(
     <><h1>My Car name is:{taxi.Name}</h1>
-     <h1>Color is:{taxi.Colorcolor} </h1>
+     <h1>Color is:{taxi.Color} </h1>
       <h1>company name is: {taxi.Company}</h1>
       <h1>Car's model {taxi.Model}</h1>
+     <button type="button" onClick={updateColor}>Update Color</button>
      </>
 );
 }
