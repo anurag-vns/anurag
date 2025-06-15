7593294266608d82620c678618082367891013d3
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-15 17:20:05 +0530

    find min value using usestate
    find smallest value between two number

--- src/Maximum.js
+++ src/Maximum.js
@@ -3,25 +3,21 @@ function Maximum() {
 
     const [a, setA] = useState(0);
     const [b, setB] = useState(0);
-    const [max, setMax] = useState((a>b)? a:b);
+    const [min, setMin] = useState((a<b)? a:b);
 
     return (
         <>
             <h1>A: {a}</h1>
             <h1>B:{b}</h1>
-            <h1>Max Value is:{max ? a : b}</h1>
+            <h1>Min Value is:{min ? a : b}</h1>
            
 
 
-            <button onClick={() => setA(a + 1)}>Increment</button>
-            <button onClick={() => setB(b + 1)}>Dcrement</button>
-            <button onClick={() => setMax(a > b)?a:b}>Update MaxValue</button>
-           
-
-
-
-        </>
-
-    );
+            <button onClick={() => setA(a + 1)}>First Value</button>
+            <button onClick={() => setB(b + 1)}>Second Valuse</button>
+            
+            <button onClick={() => setMin(a < b)? a:b}>Update MaxValue</button>
+            </>
+  );
 }
 export default Maximum;
