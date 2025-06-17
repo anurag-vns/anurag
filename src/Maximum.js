2a1ca3a917d9c2e9a6a2fa0bedb73478b586ef15
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-17 12:33:11 +0530

    using use state find largest in 4 number

--- src/Maximum.js
+++ src/Maximum.js
@@ -4,7 +4,8 @@ function Maximum() {
     const [a, setA] = useState('');
     const [b, setB] = useState('');
     const [c, setC] = useState('');
-    const [max, setMax] = useState((a >= b && a >= c) ? a : (b >= c) ? b : c);
+    const [d, setD] = useState('');
+    const [max, setMax] = useState((a >= b && a >= c && a>=d) ? a : (b >= c && b>=d)? b: (c>=d)?c:d);
 
     const handlechangeA = (event) => {
         setA(event.target.value);
@@ -19,17 +20,23 @@ function Maximum() {
         setC(event.target.value);
     };
 
+    const handlechangeD=(event)=>{
+setD(event.target.value);
+    };
+
     return (
         <>
             <h1>First Value: {a}</h1>
             <h1>Second Value:{b}</h1>
             <h1>Third value:{c}</h1>
+            <h1>Fourth value:{d}</h1>
             <h1>Find Largest Value:{max}</h1>
 
             <input type="text"  value={a} onChange={handlechangeA} placeholder="Enter First Number"/>
             <input type="text" value={b} onChange={handlechangeB} placeholder="Enter second number" />
             <input type="text"  value={c} onChange={handlechangeC}  placeholder="Enter third number"/>
-            <button onClick={() => setMax((a >= b && a >= c) ? a : (b >= c) ? b : c)}>Update MaxValue</button>
+            <input type="text" value={d} onChange={handlechangeD} placeholder="Enter Fourth number"/>
+            <button onClick={() => setMax((a >= b && a >= c && a>=d) ? a : (b >= c && b>=d) ? b :(c>=d)?c:d)}>Update MaxValue</button>
         </>
     );
 }
