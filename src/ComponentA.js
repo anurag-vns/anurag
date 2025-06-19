4bf710351f7258cc83cf7574c78b76d77cf8e9e2
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-19 18:33:04 +0530

    Update ComponentA.js

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,29 +1,29 @@
 import React, { use, useState } from "react";
 
 function ComponentA() {
-const [a,setA]=useState('');
-const [b,setB]=useState('');
-const [result,setResult]=useState('');
+    const [a, setA] = useState('');
+    const [b, setB] = useState('');
+    const [result, setResult] = useState('');
 
-function addnumber(){
-    setResult(Number(a)+ Number(b));
-}
+    function addnumber() {
+        setResult(Number(a) + Number(b));
+    }
 
-function subnumber(){
-    setResult(Number(a)- Number(b));
-}
+    function subnumber() {
+        setResult(Number(a) - Number(b));
+    }
 
 
     return (
         <div>
             <center>
                 <h1>Calculation:{result}</h1>
-            <input type="text" value={a} onChange={(e)=> setA(e.target.value)} placeholder="Enter First Number"/>
-            <input type=" text" value={b} onChange={(e)=> setB(e.target.value)} placeholder="Enter Second Number"/>
-            <button onClick={addnumber}>Addition</button>
-            <button onClick={subnumber}>Subtraction</button>
+                <input type="text" value={a} onChange={(e) => setA(e.target.value)} placeholder="Enter First Number" />
+                <input type=" text" value={b} onChange={(e) => setB(e.target.value)} placeholder="Enter Second Number" />
+                <button onClick={addnumber}>Addition</button>
+                <button onClick={subnumber}>Subtraction</button>
             </center>
-            
+
         </div>
     );
 }
