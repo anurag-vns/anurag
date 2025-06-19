381ec6072637c78157f28f088aa87cfebd7dbf7d
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-19 20:04:04 +0530

    screenshot of usestate example

--- src/ComponentD.js
+++ src/ComponentD.js
@@ -6,12 +6,12 @@ function ComponentD() {
     const [list, setList] = useState('add');
     const [result, setResult] = useState('0');
 
-    function handlechange(){
-        if(list==='add'){
-            setResult(Number(a)+Number(b));
+    function handlechange() {
+        if (list === 'add') {
+            setResult(Number(a) + Number(b));
         }
-        else if(list==='sub'){
-          setResult(Number(a)-Number(b));
+        else if (list === 'sub') {
+            setResult(Number(a) - Number(b));
         }
     }
 
@@ -20,15 +20,15 @@ function ComponentD() {
             <center>
                 <h1>Calculation: {result}</h1>
                 <input type="text" value={a} onChange={(e) => setA(e.target.value)} placeholder="Enter Frist Number" />
-                 <input type="text" value={b} onChange={(e) => setB(e.target.value)} placeholder="Enter second Number" /><br/><br/>
+                <input type="text" value={b} onChange={(e) => setB(e.target.value)} placeholder="Enter second Number" /><br /><br />
 
-                 
-                 <select value={list} onChange={(e)=> setList(e.target.value)} onClick={handlechange}>
+
+                <select value={list} onChange={(e) => setList(e.target.value)} onClick={handlechange}>
                     <option value={Text}>Select </option>
                     <option value={"add"}>Add</option>
                     <option value={"sub"}>Subtract</option>
-                 </select><br/><br>
-                 </br>
+                </select><br /><br>
+                </br>
             </center>
         </div>
 
