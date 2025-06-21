50ef4a900f5d8382f00c77135d1051af02b6a910
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-21 20:12:31 +0530

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Message(props){
     return(
-        <h1>Hello{props.name} my age is:{props.age}</h1>
+        <h1>Hello{props.name} my age is:{props}</h1>
     );
 }
 export default Message;
